a892fee0cbd482454077a7ffafc053186a39db2f
Jonathan <jschack94@gmail.com>
2020-08-07 13:11:14 -0500

    start

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-  import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Router } from "react-router-dom";
-import history from "./history";
+import './stylesheets/index.css';
+import App from './components/App';
 
 ReactDOM.render(
-  <Router history={history}>
+  <React.StrictMode>
     <App />
-  </Router>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
